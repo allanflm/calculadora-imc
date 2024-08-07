1869fbf139a4714c1e3b66d7b0b927798c617dad
Allan <allanfelipedk@gmail.com>
2024-08-07 20:14:24 -0300

    finishing imc app

--- app/src/main/java/com/allanfelipe/calculadoraimc/ResultadoActivity.kt
+++ app/src/main/java/com/allanfelipe/calculadoraimc/ResultadoActivity.kt
@@ -1,13 +1,21 @@
 package com.allanfelipe.calculadoraimc
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ResultadoActivity : AppCompatActivity() {
+
+    private lateinit var textPeso: TextView
+    private lateinit var textAltura: TextView
+    private lateinit var resultado: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_resultado)
@@ -15,6 +23,32 @@ class ResultadoActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        textPeso = findViewById(R.id.text_peso)
+        textAltura = findViewById(R.id.text_altura)
+        resultado = findViewById(R.id.text_resultado)
+
+        val bundle  = intent.extras
+        if(bundle != null){
+            val peso = bundle.getDouble("peso")
+            val altura = bundle.getDouble("altura")
+
+            textPeso.text = "Peso informado $peso kg."
+            textAltura.text = "Altura informada $altura m."
+
+            val imc = peso / (altura * altura)
+
+            resultado.text = if(imc < 18.5){
+                "Baixo"
+            }else if(imc in 18.5.. 24.9) {
+              "Normal"
+
+            } else if (imc in 25.0..29.9) {
+                "Sobrepeso"
+            } else {
+                "Obeso"
+            }
+
         }
     }
 }
